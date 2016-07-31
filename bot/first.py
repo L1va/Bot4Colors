06a5f30b8204e5296c856a99c33fe4fbcc8736bb
Mike Ivanou <mi@gojuno.com>
2016-07-31 13:45:28 +0300

    max

--- bot/first.py
+++ bot/first.py
@@ -1,22 +1,32 @@
 
-
+from collections import Counter
 games = dict()
 
 class Game:
     def __init__(self, board):
         print(board)
         self.count = board["figures_count"]
+        self.cnt = Counter()
+        for row in board["cells"]:
+            self.cnt.update(row)
         self.used = []
 
 
 def max_count(id, color):
     game = games[id]
-    for i in range(game.count):
-        if i not in game.used:
-            game.used.append(i)
-            return i
-    return 0
+    fig, count = game.cnt.most_common(1)[0]
+    use(game, fig)
+    return fig
+    # for i in range(game.count):
+    #     if i not in game.used:
+    #         game.used.append(i)
+    #         return i
+    # return 0
+
+def use(game, fig):
+    del game.cnt[fig]
 
 def enemy_step(id, fig, color):
     game = games[id]
-    game.used.append(fig)
+    use(game, fig)
+    #game.used.append(fig)
