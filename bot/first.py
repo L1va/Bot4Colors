a933dc9c73435e5c3c36b5f6ca1a7ab7221a09d5
Aliaksiej Homza <aliaksei.homza@gmail.com>
2016-07-31 15:17:38 +0300

    code clean

--- bot/first.py
+++ bot/first.py
@@ -1,40 +1,36 @@
-
-from collections import Counter
 games = dict()
-
 
 
 class Game:
     def __init__(self, board):
-        #self.count = board["figures_count"]
+        # self.count = board["figures_count"]
         self.cells = dict()
-        #self.not_used = dict()
+        # self.not_used = dict()
         cells = board["cells"]
-        for i in range (len(cells)):
-            row = cells[i]
-            for j in range(len(row)):
-                cur_id = row[j]
+        for i, row in enumerate(cells):
+            for j, cur_id in enumerate(row):
                 if cur_id in self.cells:
                     cur_cell = self.cells[cur_id]
                 else:
-                    cur_cell = Cell(cur_id,self)
+                    cur_cell = Cell(cur_id, self)
                     self.cells[cur_id] = cur_cell
-                    #self.not_used[cur_id] = True
-                if i!=0:
+                    # self.not_used[cur_id] = True
+                if i != 0:
                     add_neigh(cells[i-1][j], cur_id, cur_cell, self.cells)
-                if j!=0:
+                if j != 0:
                     add_neigh(cells[i][j-1], cur_id, cur_cell, self.cells)
-                if i%2==0 and i>0 and j>0:
+                if i % 2 == 0 and i > 0 and j > 0:
                     add_neigh(cells[i-1][j-1], cur_id, cur_cell, self.cells)
-                elif i%2!=0 and i>0 and j<len(row)-1:
+                elif i % 2 != 0 and i > 0 and j < len(row) - 1:
                     add_neigh(cells[i-1][j+1], cur_id, cur_cell, self.cells)
 
 
-def add_neigh(id, cur_id,cur_cell, cells):
-    if id!=cur_id:
+def add_neigh(id, cur_id, cur_cell, cells):
+    if id != cur_id:
         cur_cell.add(id)
         left_c = cells[id]
         left_c.add(cur_id)
+
 
 class Cell:
     def __init__(self, id, game):
@@ -47,20 +43,21 @@ class Cell:
         self.neigh[id] = True
 
     def can_color(self, color):
-        for id,v in self.neigh.items():
+        for id, v in self.neigh.items():
             c = self.game.cells[id]
             if c.color == color:
                 return False
         return True
 
+
 def max_count(id, color):
     game = games[id]
-    for id,c in game.cells.items():
-        if c.color==-1 and c.can_color(color):
+    for id, c in game.cells.items():
+        if c.color == -1 and c.can_color(color):
             c.color = color
             return id
+
 
 def enemy_step(id, fig, color):
     game = games[id]
     game.cells[fig].color = color
-
