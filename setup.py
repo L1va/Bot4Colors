9c325663c1d5afa885bd279d792e71cad6ba120d
Aliaksiej Homza <aliaksei.homza@gmail.com>
2016-07-31 11:25:19 +0300

    Initial commit

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='bot',
+    version='0.0.1',
+    author='GomZik',
+    author_email='gomzik000@gmail.com',
+    packages=find_packages())
