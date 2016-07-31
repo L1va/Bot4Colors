560b0d0b066c1cf212cc6756866ddb62e23f3fef
Aliaksiej Homza <aliaksei.homza@gmail.com>
2016-07-31 16:35:08 +0300

    Use maximum

--- bot/first.py
+++ bot/first.py
@@ -9,6 +9,7 @@ class Game:
         cells = board["cells"]
         for i in range(board["figures_count"]):
             self.cells[i] = Cell(i,self)
+            self.cells.inc()
 
         for i, row in enumerate(cells):
             for j, cur_id in enumerate(row):
@@ -20,12 +21,12 @@ class Game:
                     neigh_id = cells[i][j-1]
                     add_neigh(self.cells[neigh_id], cur_cell)
                 if i % 2 == 0 and i > 0 and j > 0:
-                    neigh_id = cells[i-1][j-1]                    
+                    neigh_id = cells[i-1][j-1]
                     add_neigh(self.cells[neigh_id], cur_cell )
                 elif i % 2 != 0 and i > 0 and j < len(row) - 1:
-                    neigh_id = cells[i-1][j+1]                    
+                    neigh_id = cells[i-1][j+1]
                     add_neigh(self.cells[neigh_id], cur_cell )
-        
+
 def add_neigh(cell, cur_cell):
     if cell.id != cur_cell.id:
         cur_cell.add(cell.id)
@@ -39,6 +40,10 @@ class Cell:
         # self.neigh = dict()
         self.neigh = set()
         self.color = -1
+        self.count = 0
+
+    def inc(self):
+        self.count += 1
 
     def add(self, id):
         self.neigh.add(id)
@@ -55,10 +60,16 @@ class Cell:
 
 def max_count(id, col):
     game = games[id]
+    maximum = None
+
     for id, c in game.cells.items():
         if c.can_color(col):
-            #game.cells[id].color = col
-            return id
+            if maximum is None:
+                maximum = c
+            else:
+                if maximum.count < c.count:
+                    maximum = c
+    return maximum.id
 
 
 def enemy_step(id, fig, col):
