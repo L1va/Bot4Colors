b9cb433b45077f7bb468b301609c75f3be6c79e4
Aliaksiej Homza <aliaksei.homza@gmail.com>
2016-07-31 15:10:30 +0300

    fix logs in production mode

--- bot/__init__.py
+++ bot/__init__.py
@@ -1,1 +1,12 @@
+import logging
+import sys
+
 from bot.web import app
+
+
+def setup_logging():
+    root_logger = logging.getLogger()
+    root_logger.setLevel('DEBUG')
+    handler = logging.StreamHandler(sys.stdout)
+    handler.setLevel('DEBUG')
+    root_logger.addHandler(handler)
--- bot/__main__.py
+++ bot/__main__.py
@@ -1,19 +1,8 @@
 import os
-import sys
-import logging
 
 from bot.web import app
 
 
-def setup_logging():
-    root_logger = logging.getLogger()
-    root_logger.setLevel('DEBUG')
-    handler = logging.StreamHandler(sys.stdout)
-    handler.setLevel('DEBUG')
-    root_logger.addHandler(handler)
-
-
 if __name__ == '__main__':
-    setup_logging()
     PORT = os.environ.get('PORT', 8000)
     app.run(port=PORT, debug=True)
